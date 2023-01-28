53ffca61c8674a96c394d7d0172cf135d3fa7b1a
Vito Medlej <rip.vitto@gmail.com>
2023-01-28 11:30:42 +0200

    QuickCart Ui change

--- src/Components/Drawer/QuickCart.tsx
+++ src/Components/Drawer/QuickCart.tsx
@@ -16,7 +16,6 @@ import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
 import CartProduct from '../Products/CartProduct';
 import Link from 'next/link';
 
-
 export default function TemporaryDrawer() {
 
     const [cartOpen,
@@ -44,12 +43,12 @@ export default function TemporaryDrawer() {
                         display: 'flex',
                         flexDirection: 'row',
                         mx: '1em',
-                        borderBottom : '1px solid #00000014',
+                        borderBottom: '1px solid #00000014',
                         justifyContent: 'space-between'
                     }}>
                         <h2
                             style={{
-                            fontWeight: '500',
+                            fontWeight: '500'
                         }}>
                             Your Cart
                         </h2>
@@ -60,18 +59,49 @@ export default function TemporaryDrawer() {
                             }}/>
                         </IconButton>
                     </Box>
-                    <Box sx={{maxHeight:'350px',overflowY:'scroll'}} >
-                            <CartProduct/>
-                            <CartProduct/>
-                            <CartProduct/>
-                            <CartProduct/>
-                            <CartProduct/>
+                    <Box
+                        sx={{
+                        maxHeight: '350px',
+                        overflowY: 'scroll'
+                    }}>
+                        <CartProduct/>
+                        <CartProduct/>
+                        <CartProduct/>
+                        <CartProduct/>
+                        <CartProduct/>
                     </Box>
-                    <Box sx={{my:1}}>
-                       <Button sx={{color:'white',background:'red',borderRadius:'6px',p:1,px:2,fontSize:'10px'}}>Checkout</Button>
-                       <Button sx={{color:''}}>Continue Shopping</Button>
+                    <Divider></Divider>
+                    <Box
+                        sx={{
+                        my: 2,
+                        mx: 1
+                    }}>
+                        <Button
+                            sx={{
+                            color: 'white',
+                            background: '#cf3030',
+                            ':hover': {
+                                background: '#cf3030'
+                            },
+                            borderRadius: '6px',
+                            py: 1,
+                            px: 2,
+                            fontSize: '10px'
+                        }}>Checkout</Button>
+                        <Button
+                            variant='outlined'
+                            sx={{
+                            color: '#cf3030',
+                            borderRadius: '6px',
+                            ml: 1,
+                            py: .9,
+                            px: 2,
+                            borderColor: ' #cf3030',
+                            ':hover':{  borderColor: ' #cf3030',background:'#cf3030',color:'white'},
+                            fontSize: '10px'
+                        }}>Continue Shopping</Button>
                     </Box>
- 
+
                 </Box>
             </Drawer>
         </div>
