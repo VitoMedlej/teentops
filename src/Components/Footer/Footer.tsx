d441b673cc3782b74bbc5d14af68178b96b1f131
vito medlej <vito.medlej@gmail.com>
2023-02-09 13:19:43 +0300

    changed footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,99 +1,73 @@
-import {Box, Typography, Divider, Grid} from '@mui/material';
-
 import Link from 'next/link';
-function Copyright() {
-    return (
-<>
-        <Divider/>
-        <Box
-        maxWidth='lg' 
-        sx={{
-            textAlign:'center',
-            margin : '1em auto'
-        }}>
-            
+// import Logo from '../../assets/icons/logo';
 
-            <Typography variant='h1' fontSize='1em'>
-                {'Done By Vito Medlej @ '}
-                <Link color="inherit" href="/">
-                    elvito.netlify.app
-                </Link>{' '} {new Date().getFullYear()}
-                {'.'}
-            </Typography>
-        </Box>
-        </>
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <div className="container">
+        <div className="site-footer__top">
+          <div className="site-footer__description">
+          <div className='logos cursor'>
 
-    );
-}
-const styles = {
-    mt: '1em',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '15px',
-    flexWrap: 'wrap',
-}
+<Link href="/" >
+  {/* <a><h1 className="site-logo"><Logo />E-Shop</h1></a> */}
+  {/* https://res.cloudinary.com/dwxm8f25f/image/upload/v1675357773/logo_ghli5e.jpg */}
+  {/* https://res.cloudinary.com/dwxm8f25f/image/upload/v1675713948/logo_sktnut_1_jwy2hk.png */}
+  <img className='img' src='https://res.cloudinary.com/dwxm8f25f/image/upload/v1675713948/logo_sktnut_1_jwy2hk.png' alt="" />
+</Link>
+</div>
+            <p className='footer-p'>0096171330473
+مستحضرات طبيعية 100 %
+شعارنا العودة إلى الطبيعة
+مبيع
+ جملة ومفرق + تسليم صفحات الاونلاين
+ </p>
+            {/* <ul className="site-footer__social-networks">
+              <li><a href="https://www.facebook.com/profile.php?id=100063581229923" rel="noreferrer" target='_blank'><i className="icon-facebook"></i></a></li>
+              <li><a href="#"><i className="icon-twitter"></i></a></li>
+              <li><a href="#"><i className="icon-linkedin"></i></a></li>
+              <li><a href={`${process.env.NEXT_PUBLIC_INSTA}`}rel="noreferrer" target='_blank' ><i className="icon-instagram"></i></a></li>
+              <li><a href="#"><i className="icon-youtube-play"></i></a></li>
+            </ul> */}
+          </div>
 
-function Footer() {
-    return (
-        <Box >
-            <Divider/>
-            <Box
-                sx={{
-                width: '100%',
-                minHeight: '200px',
-                display: 'flex',
-           
-            }}>
-                <Grid
-                    sx={{
-                    gap: '1.5em',
-                    mx: {
-                        xs: '3vw',
-                        lg: 'auto'
-                    },
-                    my: '2em'
-                }}
-                    maxWidth='lg'
-                    container>
-                    <Grid item xs={12} sm={6} md={5}>
-                        <Typography variant='h1' fontSize='1.4em' fontWeight='400'>About</Typography>
+          <div className="site-footer__links">
+            <ul>
+              <li className='link-title'>Shopping online</li>
+              <li><a href="#">Order Status</a></li>
+              <li><a href="#">Shipping and Delivery</a></li>
+              <li><a href="#">Returns</a></li>
+              <li><a href="#">Payment options</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul>
+              <li className='link-title'>Information</li>
+              <li><a href="#">Gift Cards</a></li>
+              <li><a href="#">Find a store</a></li>
+              <li><a href="#">Newsletter</a></li>
+              <li><a href="#">Bacome a member</a></li>
+              <li><a href="#">Site feedback</a></li>
+            </ul>
+            <ul>
+              <li className='link-title'>Contact</li>
+              
+              <li><a href="#">البازورية , Tyre, Lebanon</a></li>
+              <li><a href="#">simplebeirut@gmail.com</a></li>
+              <li><a href={'https://wa.me/96171330473'}>Phone/Whatsapp:{` +961 71330473`}</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+      <div className="site-footer__bottom">
+        <div className="container">
+          <p>Website Updated by 
+            <a style={{color:'blue'}} href={`${process.env.NEXT_PUBLIC_US}`}>{' '}Simple Beirut Web</a></p>
+        </div>
+      </div>
+    </footer>
+  )
+};
 
-                        <Box sx={styles}>
-                            <Typography variant='h3' fontSize='1em'>
-                            elvito-planet is a travel guide aimed to to help more people from more backgrounds experience the joy of exploration. This website design was inspired by various travel website on the internet and I don&apos;t claim to own any of the content whatsoever.  
-                            </Typography>
-                        </Box>
 
-                    </Grid>
-                    <Grid item xs={12} sm={4} md={3}>
-                        <Typography variant='h1' fontSize='1.4em' fontWeight='400'>Links</Typography>
-
-                        <Box className='link' sx={styles}>
-                            <Link 
-                            className='FooterLink'
-                            href='/'
-                            >Home</Link>
-                            <Link href='/contact'>Contact</Link>
-                         
-                            <Link href='/contact'>Contact</Link>
-                        </Box>
-
-                    </Grid>
-                    <Grid item xs={12} sm={3} md={3}>
-                        <Typography variant='h1' fontSize='1.4em' fontWeight='400'>Contact</Typography>
-                        <Box className='link' sx={styles}>
-                            <a target='_blank' rel="noreferrer" href='https://github.com/VitoMedlej'>Github</a>
-                            <Typography variant='h1' fontSize='1em'>vito.medlej@gmail.com</Typography>
-                            <Typography variant='h1' fontSize='1em'>Lebanon/beirut</Typography>
-                            <Typography variant='h1' fontSize='1em'>+961/81826445</Typography>
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Box>
-            <Copyright/>
-        </Box>
-
-    );
-
-}
 export default Footer
