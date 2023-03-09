8e9ff14f0f61bef1c83117b52c6eda948c0c8b0c
vito medlej <vito.medlej@gmail.com>
2023-03-09 20:40:56 +0300

    api edits

---
+++ pages/api/category/[index].js
@@ -0,0 +1,22 @@
+// domain/api/category/ 
+
+
+import main from "../database/connection";
+import UserModel from "../database/schema";
+
+
+export default async function get_Users(req, res){
+const name = req.params.category
+
+
+  main().catch(error => console.error(error));
+  const create = new UserModel({name: 'Jane Doe', price: 120 });
+  create.save().then(() => {
+    console.log('create: ', create);
+    return    res.status(200).json(create)
+  })
+
+ 
+
+
+}
--- src/database/schema.js
+++ src/database/schema.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const itemsDb = new mongoose.Schema({
-name: String,
+title: String,
 price: Number
 });
-
+//title, id, price, description, category, additionalInfo, img (main) , images (array of images)
 const itemsDbs = mongoose.model('itemsDb', itemsDb)
 // const itemsDbs = mongoose.model('itemsDb') || mongoose.model('itemsDb', itemsDb)
 export default itemsDbs;
