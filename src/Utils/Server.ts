ee2a495338276d04e9bcd2b16f6bc3ed83bec817
Vito Medlej <vito.medlej@gmail.com>
2023-03-18 15:41:56 +0200

    Update server.ts

--- src/Utils/Server.ts
+++ src/Utils/Server.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : `${process.env.NEXT_PUBLIC_URL || 'https://powerhouse-lb.netlify.app'}`;
+export const server = dev ? 'http://localhost:3000' : `${process.env.NEXT_PUBLIC_URL || 'https://powerhouse-lb.com'}`;
