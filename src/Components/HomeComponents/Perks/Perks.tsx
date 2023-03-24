7fcade08fd2315e2c4a367e974518d9856639acb
Your Name <you@example.com>
2023-03-24 12:12:22 +0200

    connection push

--- src/Components/HomeComponents/Perks/Perks.tsx
+++ src/Components/HomeComponents/Perks/Perks.tsx
@@ -1,29 +1,32 @@
-import {Box, Typography} from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
-import HeadphonesIcon from '@mui/icons-material/Headphones';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 const Perks = () => {
     return (
         <Box
-        className='flexed bg'
-        sx={{
-            my:'3em',
-            flexWrap:'wrap',
-            // background:'#f0f0f0',
-            py:'1em',
-            justifyContent: 'space-evenly',
-            width: '100%',
-            color:'white'
-        }}>
-            <Box
-            className='flexed'
-                sx={{
-                textAlign: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                pb:{xs:'1em',sm:0}
+            className='flexed bg'
+            sx={{
+                my: '3em',
+                flexWrap: 'wrap',
+                // background:'#f0f0f0',
+                py: '1em',
+                justifyContent: 'space-evenly',
+                width: '100%',
+                color: 'white'
             }}>
+            <Box
+                className='flexed'
+                sx={{
+                    textAlign: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    pb: { xs: '1em', sm: 0 }
+                }}>
                 <span style={{
-                    fontWeight:'600',
+                    fontWeight: '600',
                     fontSize: '1.1em'
                 }}>
                     We are Always Here To Help
@@ -31,50 +34,128 @@ const Perks = () => {
                 <span
                     className='white'
                     style={{
-                    fontSize: '.8em'
-                }}>Reach out to us through any of these support channels</span>
+                        fontSize: '.8em'
+                    }}>Reach out to us through any of these support channels</span>
             </Box>
-            <Box sx={{display:'flex',flexWrap:'wrap',justifyContent: 'center',gap:'1em'}}>
-                {[{title:'Phone Number',value:"70873045",isLink:false}, {isLink:true,title:'Instagram',value:'https://www.instagram.com/power_house_european/'}, {isLink:true,title:'Facebook',value:'https://www.facebook.com/online.european.outlet/'}].map(i => {
-                    return <Box 
-                    key={i.title}
-                        sx={{
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '1em' }}>
+                {/* {[{title:'Phone Number',value:"70873045",isLink:false}, {isLink:true,title:'Instagram',value:'https://www.instagram.com/power_house_european/'}, {isLink:true,title:'Facebook',value:'https://www.facebook.com/online.european.outlet/'}].map(i => {
+                    return 
+                })} */}
+
+                <Box
+
+                    sx={{
                         display: 'flex',
-                        gap:'.5em',
-                        flexDirection: 'row' 
+                        gap: '.5em',
+                        flexDirection: 'row'
                     }}>
-                        <Box className='flexed'>
-                            <HeadphonesIcon sx={{color:'#00000073'}}/>
-                        </Box>
-                        <Box
-                            sx={{
+                    <Box className='flexed'>
+                        <InstagramIcon sx={{ color: '#fff' }} />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}>
+                        {<a className='white' target={`_blank`} rel='noreferrer' style={{textDecoration:"none"}} href={"https://www.instagram.com/power_house_european/"}>
+
+
+                            <Typography
+                                sx={{
+                                    fontSize: '1.1em'
+                                }}>Instagram</Typography>
+                        </a>
+
+                        }
+                    </Box>
+
+                </Box>
+
+
+
+
+
+                <Box
+
+                    sx={{
+                        display: 'flex',
+                        gap: '.5em',
+                        flexDirection: 'row'
+                    }}>
+                    <Box className='flexed'>
+                        <FacebookIcon sx={{ color: '#fff' }} />
+                    </Box>
+                    <Box
+                        sx={{
                             display: 'flex',
                             alignItems: 'center',
                             justifyContent: 'center',
                             flexDirection: 'column'
                         }}>
-                            {i.isLink? <a className='white' target={`_blank`} rel='noreferrer' href={i.value}>
-                                
-                            
-                            <Typography
-                            sx={{
-                                fontSize: '1.1em'
-                            }}>{i.title}</Typography>
-                            </a> 
-                        :
-                        <>
-                        <Typography
-                            sx={{
-                                fontSize: '.75em'
-                            }}>{i.title}</Typography>
-                        
-                        <Typography fontSize='1.1em' fontWeight='600'>{i.value}</Typography>
-                            </>
-                        }
-                        </Box>
+                        {<a className='white' target={`_blank`} style={{textDecoration:"none"}} rel='noreferrer' href={"https://www.facebook.com/online.european.outlet/"}>
 
+
+                            <Typography
+                                sx={{
+                                    fontSize: '1.1em'
+                                }}>Facebook</Typography>
+                        </a>
+
+                        }
                     </Box>
-                })}
+
+                </Box>
+
+
+
+
+
+
+                <Box
+
+                    sx={{
+                        display: 'flex',
+                        gap: '.5em',
+                        flexDirection: 'row'
+                    }}>
+                    <Box className='flexed'>
+                        <WhatsAppIcon sx={{ color: '#fff' }} />
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            flexDirection: 'column'
+                        }}>
+                        {<a className='white' target={`_blank`} style={{textDecoration:"none"}} rel='noreferrer' href={"https://wa.me/96170873045"}>
+
+
+                            <Typography
+                                sx={{
+                                    fontSize: '1.1em'
+                                }}>Whatsapp</Typography>
+                        </a>
+
+                        }
+                    </Box>
+
+                </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </Box>
         </Box>
     )
