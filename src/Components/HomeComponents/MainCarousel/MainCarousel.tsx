742bcc5ca43baf04fdf9c573c38d0c578a07ac10
Vito Medlej <rip.vitto@gmail.com>
2023-07-05 11:55:03 +0300

    Update MainCarousel.tsx

--- src/Components/HomeComponents/MainCarousel/MainCarousel.tsx
+++ src/Components/HomeComponents/MainCarousel/MainCarousel.tsx
@@ -37,6 +37,7 @@ const MainCarousel = () => {
 
                 {[{img:`https://ucarecdn.com/462ad62c-137f-41e2-8a1d-ccada165f454/GreenandWhiteIllustrationMinimalistFastDeliveryFacebookCoverS4971.png`}
                 ,
+                  {img:`https://ucarecdn.com/59eb52e0-d54d-473e-8c4e-95e488a93c98/356646036_282381760977275_2221277271188922948_n.jpg`},
                   {img:`https://ucarecdn.com/66b8154e-a03e-4b9f-a161-5695638874ec/GrayandGreenElectronicAdvertisementBannerFacebookAds21.png`},
                   {img:`https://ucarecdn.com/a59f0de3-9255-44cd-b18c-c2347b684754/GrayandGreenElectronicAdvertisementBannerFacebookAds111.png`},
                 {img:`https://ucarecdn.com/bd4333ac-e788-487f-b948-cddb3b76075f/fan.JPG`},
